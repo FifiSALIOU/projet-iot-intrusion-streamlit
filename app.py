0e95311529c8437d86c1177052dd7faa51cb6605
FifiSALIOU <fatougueyendong@esp.sn>
2025-06-16 13:41:57 +0000

    ✅ Correction

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if models is not None:
     selected_model = st.selectbox(
         "Sélectionnez le modèle à utiliser:",
         list(models.keys())
-        key="model_simple"
+
     )
     model = models[selected_model]
 
@@ -201,7 +201,7 @@ if models is not None:
                 selected_model = st.selectbox(
                     "Sélectionnez le modèle à utiliser:",
                     list(models.keys())
-                    key="model_batch"
+                    
                 )
                 model = models[selected_model]
                 
